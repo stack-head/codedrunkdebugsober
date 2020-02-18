973133c9b978cb1b2425e2de0bfbe21424426913
James O'Carroll <james.ocarroll@stackhead.com>
2020-02-17 22:52:11 -0600

    Demo xero updates

--- zmq/README.md
+++ zmq/README.md
@@ -1,4 +1,17 @@
 # Demo Xero
 
-demo_dero and the xero library include a simple version of the ZMQ Paranoid Pirate pattern, it's primarily meant for teaching/demonstration.
+See https://codedrunkdebugsober.com/pyzmq-paranoid-pirate/ for more explanation.
+
+demo_xero and the xero library include a simple version of the ZMQ Paranoid Pirate pattern, it's primarily meant for teaching/demonstration.
 There are a few departures from the true Paranoid Pirate pattern. The primary difference is the example only supports one worker -this is done for clarity, a full example would support multiple workers. Additionally, the full paranoid pirate would support sending out multiple messages before a response arrives. This implementation only supports one response at a time, this is done to enforce an RPC style interface.
+
+run_demo_xero_in_docker_env.sh will build docker containers to demonstrate running this sample in a controlled docker environment.
+
+If you want to run the samples directly on your dev machine, install both xero and demo_xero via:
+pip3 install -e ./xero
+pip3 install -e ./demo_xero
+
+In one terminal, run the worker:
+./bin/demo_xerouniworker tcp://127.0.0.1:5550
+
+In a second terminal, run any of the commands in run_examples.sh
--- zmq/demo_xero/run_examples.sh
+++ zmq/demo_xero/run_examples.sh
@@ -3,12 +3,24 @@
 set -eu
 set -o pipefail
 
+# Simplest example-call RPC with no arguments and get single string as a reply
 ./bin/demo_xerouniclient tcp://127.0.0.1:5550 ping
-./bin/demo_xerouniclient tcp://127.0.0.1:5550 invalid_request
-./bin/demo_xerouniclient tcp://127.0.0.1:5550 compare --args '"uno", "dos"'
-./bin/demo_xerouniclient tcp://127.0.0.1:5550 compare --kwargs '"str1":"uno", "str2":"dos"'
+
+# Similar, call RPC with no arguments and ensure the "reply" comes in, but no data returned
 ./bin/demo_xerouniclient tcp://127.0.0.1:5550 return_none
-./bin/demo_xerouniclient tcp://127.0.0.1:5550 take_too_long
+
+# Call an RPC that just does an equality comparison. Simple example to demonstrate how to pass arguments to worker RPC
+./bin/demo_xerouniclient tcp://127.0.0.1:5550 compare --args '"apple", "orange"'
+
+# Effectively the same thing, but showing how to use named arguments.
+./bin/demo_xerouniclient tcp://127.0.0.1:5550 compare --kwargs '"str1":"apple", "str2":"orange"'
+
+# Call an RPC that doesn't actually exist, and make sure the worker is able to handle the problem cleanly while returning
+# the actual exception that is raised.
+./bin/demo_xerouniclient tcp://127.0.0.1:5550 invalid_request
+
+# The slow_ functions are serviced by an "actor" model. slow_succeed will succeed after the provided timeout,
+# slow_fail will fail after the provided timeout.
 ./bin/demo_xerouniclient tcp://127.0.0.1:5550 slow_succeed --args 5 --timeout 20
 ./bin/demo_xerouniclient tcp://127.0.0.1:5550 slow_fail --args 5 --timeout 20
 
